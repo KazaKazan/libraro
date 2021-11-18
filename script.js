78b178edf4f4fe7f0715326b05f8649b507a6c0a
KazaKazan <ahmetfiratusta@hotmail.com>
2021-11-18 15:16:54 +0300

    Complete book adding functionality.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const addButton = document.getElementById("addButton")
+const acceptButton = document.getElementById("acceptButton")
+const cancelButton = document.getElementById("cancelButton")
+const addPrompt = document.getElementById("addPrompt")
+const titleField = document.getElementById("titleField")
+const authorField = document.getElementById("authorField")
+const pageField = document.getElementById("numberField")
+const statusField = document.getElementById("statusField")
+
 const library = [];
 
 function Book(title,author,pages,read,ID){
@@ -8,69 +17,100 @@ function Book(title,author,pages,read,ID){
     this.ID = ID;
 }
 
-const bookA = new Book("Book A","Author A",100,false,0);
-const bookB = new Book("Book B","Author B",250,false,1);
-const bookC = new Book("Book C","Author C",100,true,2);
+function clearFields () {
+    titleField.value = null;
+    authorField.value = null;
+    pageField.value = null;
+    statusField.balue = false;
+}
 
-//for(let i = 0; i < 10; i++){
-//    library.push(bookA)
-//    library.push(bookB)
-//    library.push(bookC)
-//}
-
-
-function displayBooks () {
-    const bookShelf = document.getElementById("content");
+function displayEmpty () {
     const emptyContainer = document.getElementById("emptyContainer");
+
+    if (library.length === 0) {
+        emptyContainer.classList.remove("hidden")
+    }
+    else {
+        emptyContainer.classList.add("hidden")
+    }
+};
+
+
+function displayBook (book) {
+    library.push(book);
+
+    const bookShelf = document.getElementById("content");
+
+    const newBook = document.createElement("div");
+    newBook.classList.add("book");
+
+    const selector = document.createElement("input")
+    selector.setAttribute("type","checkbox")
+    selector.setAttribute("width","14px")
+    newBook.appendChild(selector)
+
+    const bookName = document.createElement("p");
+    bookName.textContent = book.title;
+    bookName.classList.add("start")
+    newBook.appendChild(bookName)
+
+    const author = document.createElement("p");
+    author.textContent = book.author;
+    newBook.appendChild(author)
+
+    const numberCont = document.createElement("div")
+    numberCont.classList.add("flexJustify")
+    const pageNumber = document.createElement("p");
+    pageNumber.textContent = book.pages;
+    numberCont.appendChild(pageNumber)
+    newBook.appendChild(numberCont)
+
+    const status = document.createElement("p");
+    if(book.read){
+        status.textContent = "Read";
+    }
+    else{
+        status.textContent = "Not read";
+    }
+    status.classList.add("end")
+    newBook.appendChild(status)
+
+    newBook.setAttribute("id","bookid" + toString(book.ID))
+
+    bookShelf.appendChild(newBook)
+    displayEmpty()
+}
+
+function displayLibrary () {
 
     if (library.length > 0) {
         emptyContainer.classList.add("hidden");
 
         library.forEach(element => {
-            const newBook = document.createElement("div");
-            newBook.classList.add("book");
-
-            const selector = document.createElement("input")
-            selector.setAttribute("type","checkbox")
-            selector.setAttribute("width","14px")
-            newBook.appendChild(selector)
-
-            const bookName = document.createElement("p");
-            bookName.textContent = element.title;
-            bookName.classList.add("start")
-            newBook.appendChild(bookName)
-
-            const author = document.createElement("p");
-            author.textContent = element.author;
-            newBook.appendChild(author)
-
-            const numberCont = document.createElement("div")
-            numberCont.classList.add("flexJustify")
-            const pageNumber = document.createElement("p");
-            pageNumber.textContent = element.pages;
-            numberCont.appendChild(pageNumber)
-            newBook.appendChild(numberCont)
-
-            const status = document.createElement("p");
-            if(element.read){
-                status.textContent = "Read";
-            }
-            else{
-                status.textContent = "Not read";
-            }
-            status.classList.add("end")
-            newBook.appendChild(status)
-
-            newBook.setAttribute("id","bookid" + toString(element.ID))
-
-            bookShelf.appendChild(newBook)
+            displayBook(element)
         });
     }
+};
 
-    else{
-        emptyContainer.classList.remove("hidden");
-    }
+function addBook () {
+    const newBook = new Book(titleField.value,authorField.value,pageField.value,statusField.value,library.length);
+    
+    library.push(newBook)
+    displayBook(newBook)
+};
 
-    }
+addButton.onclick = () => {
+    addPrompt.classList.remove("hidden")
+    clearFields()
+};
 
-window.onload = displayBooks()
+acceptButton.onclick = () => {
+    addBook()
+    addPrompt.classList.add("hidden")
+};
+
+cancelButton.onclick = () => {
+    addPrompt.classList.add("hidden")
+};
+
+window.onload = displayLibrary()
