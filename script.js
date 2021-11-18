da2bf68d515211a0d7e1bdbb52a32db5d46b0dd2
KazaKazan <ahmetfiratusta@hotmail.com>
2021-11-18 13:32:47 +0300

    Complete book display function.

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ const bookA = new Book("Book A","Author A",100,false);
 const bookB = new Book("Book B","Author B",250,false);
 const bookC = new Book("Book C","Author C",100,true);
 
-library.push(bookA)
-library.push(bookB)
-library.push(bookC)
+for(let i = 0; i < 5; i++){
+    library.push(bookA)
+    library.push(bookB)
+    library.push(bookC)
+}
+
 
 function displayBooks () {
     const bookShelf = document.getElementById("content");
@@ -25,13 +28,36 @@ function displayBooks () {
         library.forEach(element => {
             const newBook = document.createElement("div");
             newBook.classList.add("book");
+
             const selector = document.createElement("input")
             selector.setAttribute("type","checkbox")
+            selector.setAttribute("width","14px")
             newBook.appendChild(selector)
+
             const bookName = document.createElement("p");
             bookName.textContent = element.title;
             bookName.classList.add("start")
             newBook.appendChild(bookName)
+
+            const author = document.createElement("p");
+            author.textContent = element.author;
+            newBook.appendChild(author)
+
+            const pageNumber = document.createElement("p");
+            pageNumber.textContent = element.pages;
+            pageNumber.setAttribute("justify-self","center")
+            newBook.appendChild(pageNumber)
+
+            const status = document.createElement("p");
+            if(element.status){
+                status.textContent = "Read";
+            }
+            else{
+                status.textContent = "Not read";
+            }
+            status.classList.add("end")
+            newBook.appendChild(status)
+
             bookShelf.appendChild(newBook)
         });
     }
